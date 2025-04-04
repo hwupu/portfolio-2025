72697e33ed0a4a0dd45441386349a269473fd24a
Pu-Hsuan Wu <hwupu.cs01@g2.nctu.edu.tw>
2025-04-05 01:57:05 +0900

    🚧 Clean up & Add Shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from "next";
 import { headers } from "next/headers";
-import Script from 'next/script'
-import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,15 +13,15 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const nonce = (await headers()).get("x-nonce") ?? '';
+  const nonce = (await headers()).get("x-nonce") ?? "";
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-      <Script id="clarity" strategy="afterInteractive" nonce={nonce}>{`(function(c,l,a,r,i,t,y){
+      <body>{children}</body>
+      <Script
+        id="clarity"
+        strategy="afterInteractive"
+        nonce={nonce}
+      >{`(function(c,l,a,r,i,t,y){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
