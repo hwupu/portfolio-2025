7dace2517ee6c01734bd134a10e4cb3425207472
Pu-Hsuan Wu <hwupu.cs01@g2.nctu.edu.tw>
2025-04-04 20:41:53 +0900

    🚧 Setup CSP nonce

---
+++ middleware.ts
@@ -0,0 +1,59 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export function middleware(request: NextRequest) {
+    const nonce = Buffer.from(crypto.randomUUID()).toString('base64')
+    const cspHeader = `
+      base-uri 'self';
+      default-src 'none';
+      connect-src 'self' https://*.clarity.ms/collect;
+      script-src 'nonce-${nonce}' 'strict-dynamic' 'self' https: 'unsafe-inline';
+      style-src 'nonce-${nonce}' 'self';
+      img-src 'self' blob: data: https://*.clarity.ms/c.gif https://c.bing.com/c.gif;
+      font-src 'self';
+      form-action 'self';
+      upgrade-insecure-requests;
+  `
+    // Replace newline characters and spaces
+    const contentSecurityPolicyHeaderValue = cspHeader
+      .replace(/\s{2,}/g, ' ')
+      .trim()
+   
+    const requestHeaders = new Headers(request.headers)
+    requestHeaders.set('x-nonce', nonce)
+   
+    requestHeaders.set(
+      'Content-Security-Policy',
+      contentSecurityPolicyHeaderValue
+    )
+   
+    const response = NextResponse.next({
+      request: {
+        headers: requestHeaders,
+      },
+    })
+    response.headers.set(
+      'Content-Security-Policy',
+      contentSecurityPolicyHeaderValue
+    )
+   
+    return response
+}
+
+export const config = {
+    matcher: [
+      /*
+       * Match all request paths except for the ones starting with:
+       * - api (API routes)
+       * - _next/static (static files)
+       * - _next/image (image optimization files)
+       * - favicon.ico (favicon file)
+       */
+      {
+        source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
+        missing: [
+          { type: 'header', key: 'next-router-prefetch' },
+          { type: 'header', key: 'purpose', value: 'prefetch' },
+        ],
+      },
+    ],
+  }
