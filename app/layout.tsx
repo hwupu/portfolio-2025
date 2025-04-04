84388ff5b2dc20620a9fb4c98e55abb2a9d4a613
Pu-Hsuan Wu <hwupu.cs01@g2.nctu.edu.tw>
2025-04-04 20:51:21 +0900

    🚧 Deploy to Vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const nonce = (await headers()).get("x-nonce");
+  const nonce = (await headers()).get("x-nonce") ?? '';
   console.log('layout', nonce);
   return (
     <html lang="en">
@@ -33,7 +33,7 @@ export default async function RootLayout({
       >
         {children}
       </body>
-      <Script strategy="afterInteractive" nonce={nonce}>{`(function(c,l,a,r,i,t,y){
+      <Script id="clarity" strategy="afterInteractive" nonce={nonce}>{`(function(c,l,a,r,i,t,y){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
