a433bace4b65f94bb6b89b66bb1d8bcc787edbff
Pu-Hsuan Wu <hwupu.cs01@g2.nctu.edu.tw>
2025-04-08 00:31:59 +0800

    🚧 Initial commit with Nuxt Security, Nuxt Script, Nuxt UI, Nuxt Eslint, and Tailwindcss.

---
+++ app.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import * as locales from "@nuxt/ui/locale";
+
+const { locale } = useI18n();
+
+const lang = computed(() => locales[locale.value].code);
+const dir = computed(() => locales[locale.value].dir);
+
+useHead({
+  htmlAttrs: {
+    lang,
+    dir,
+  },
+});
+useSeoMeta({
+  ogImage: "/og-image/cubic-center.png",
+  ogTitle: "Cubic Center",
+  ogDescription: "A portfolio website for Cubic Center",
+});
+</script>
+
+<template>
+  <UApp :locale="locales[locale]">
+    <NuxtRouteAnnouncer />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </UApp>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,74 @@
+import tailwindcss from "@tailwindcss/vite";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  typescript: {
+    typeCheck: true,
+  },
+  modules: [
+    "@nuxt/ui",
+    "@nuxt/eslint",
+    "@nuxtjs/i18n",
+    "nuxt-security",
+    "@nuxt/scripts",
+  ],
+  css: ["~/assets/css/main.css"],
+  app: {
+    head: {
+      title: "Nuxt", // default fallback title
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+        { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-touch-icon.png",
+        },
+      ],
+    },
+  },
+  scripts: {
+    registry: {
+      clarity: {
+        id: "qom3dgopw9",
+      },
+    },
+  },
+  security: {
+    headers: {
+      contentSecurityPolicy: {
+        "default-src": ["'none'"],
+        "connect-src": ["'self'", "https://*.clarity.ms/collect"],
+        "img-src": [
+          "'self'",
+          "https://*.clarity.ms/c.gif",
+          "https://c.bing.com/c.gif",
+        ],
+        "script-src": [
+          "'nonce-{{nonce}}'",
+          "'strict-dynamic'",
+          "'self'",
+          "https://*.phwu.dev",
+          "'unsafe-inline'",
+        ],
+      },
+    },
+  },
+  i18n: {
+    locales: [
+      {
+        code: "en",
+        name: "English",
+      },
+      {
+        code: "zh_tw",
+        name: "中文",
+      },
+    ],
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
