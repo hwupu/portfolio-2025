42389029a027caa43bfecf5c3b522a0a478635e4
Pu-Hsuan Wu <hwupu.cs01@nctu.edu.tw>
2025-04-08 00:31:59 +0800

    🚧 Initial commit with Nuxt Security, Nuxt Script, Nuxt UI, Nuxt Eslint, and Tailwindcss.

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const { proxy } = useScriptClarity();
+
+// noop in development, ssr
+// just works in production, client
+function agree() {
+  proxy.clarity("consent");
+}
+</script>
+<template>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <UButton @click="agree">Button</UButton>
+</template>
